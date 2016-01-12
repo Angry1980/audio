3e23e88cf9754bed041e5ee9471812d45b0a8a35
dmitry.khomutov <dmitry.khomutov@corp.mail.ru>
2016-01-12 19:16:36 +0300

    refactoring

--- src/main/java/angry1980/audio/config/CommonConfig.java
+++ src/main/java/angry1980/audio/config/CommonConfig.java
@@ -7,6 +7,7 @@ import angry1980.audio.dao.TrackDAOFileImpl;
 import angry1980.audio.dao.TrackSimilarityDAO;
 import angry1980.audio.dao.TrackSimilarityDaoInMemoryImpl;
 import angry1980.utils.FileUtils;
+import angry1980.utils.Numbered;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,11 @@ import org.springframework.core.env.Environment;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 @Configuration
 @PropertySource({"classpath:common.properties"})
@@ -37,9 +41,18 @@ public class CommonConfig {
 
     @Bean
     public TrackDAO trackDAO(){
+        Map<Long, Map<Long, Path>> files = new HashMap<>();
         Path dir = Paths.get(env.getProperty("music.input.folder"));
-        List<Path> files = FileUtils.getFiles(dir, ".mp3");
-        List<String> clusters = FileUtils.getDirs(dir).stream().map(path -> path.toString()).collect(Collectors.toList());
-        return new TrackDAOFileImpl(files, clusters);
+        List<Path> clusters = FileUtils.getDirs(dir);
+        long fileId =0;
+        for(long i = 0; i < clusters.size(); i++){
+            Map<Long, Path> t = new HashMap<>();
+            for(Path path : FileUtils.getFiles(clusters.get((int) i), ".mp3")){
+                t.put(fileId, path);
+                fileId++;
+            }
+            files.put(i, t);
+        }
+        return new TrackDAOFileImpl(files);
     }
 }
--- src/main/java/angry1980/audio/dao/TrackDAOFileImpl.java
+++ src/main/java/angry1980/audio/dao/TrackDAOFileImpl.java
@@ -13,15 +13,21 @@ public class TrackDAOFileImpl implements TrackDAO{
     private Map<Long, Track> tracks;
 
     public TrackDAOFileImpl(List<Path> files){
-        this(files, Collections.emptyList());
-    }
-
-    public TrackDAOFileImpl(List<Path> files, List<String> clusters) {
         Objects.requireNonNull(files);
         this.tracks = LongStream.range(0, files.size())
-                        .mapToObj(id -> new Track(id, files.get((int) id).toString()))
-                        .map(t -> t.setCluster(getCluster(clusters, t.getPath())))
-                        .collect(Collectors.toMap(t -> t.getId(), Function.identity()))
+                .mapToObj(id -> new Track(id, files.get((int) id).toString()))
+                .collect(Collectors.toMap(t -> t.getId(), Function.identity()))
+        ;
+
+    }
+
+    public TrackDAOFileImpl(Map<Long, Map<Long, Path>> files) {
+        Objects.requireNonNull(files);
+        this.tracks = files.entrySet().stream()
+                .flatMap(entry -> entry.getValue().entrySet().stream()
+                                    .map(file -> new Track(file.getKey(), file.getValue().toString()))
+                                    .map(t -> t.setCluster(entry.getKey()))
+                ).collect(Collectors.toMap(t -> t.getId(), Function.identity()))
         ;
     }
 

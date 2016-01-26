54f988b3d3a04ab8f973619711171c2c8688fa15
dmitry.khomutov <dmitry.khomutov@corp.mail.ru>
2016-01-26 17:20:54 +0300

    false negative value fix

--- neo4j/src/main/java/angry1980/audio/neo4j/FingerprintTypeFalseNegativeQuery.java
+++ neo4j/src/main/java/angry1980/audio/neo4j/FingerprintTypeFalseNegativeQuery.java
@@ -5,9 +5,9 @@ import angry1980.audio.model.FingerprintType;
 public class FingerprintTypeFalseNegativeQuery extends FingerprintTypeQuery{
 
     private static final String QUERY = "match (track1:TRACK)-[:IS]->(cluster1)"
-            + " where not (track1)-[:SIMILAR{type:{fingerprintType}}]->(:TRACK)-[:IS]->(:CLUSTER{id:cluster1.id})"
-            //+ " return track1.id"
-            + " return count(DISTINCT(track1.id)) as result, false as r"
+            + ", (track2:TRACK)-[:IS]->(:CLUSTER{id:cluster1.id})"
+            + " where track1 <> track2 and not (track1)-[:SIMILAR{type:{fingerprintType}}]->(track2)"
+            + " return count(track1)/2 as result, false as r"
             ;
 
     public FingerprintTypeFalseNegativeQuery(FingerprintType type) {

5289df6e4fbc4bdcab2a0f720a82df715874d0c6
dmitry.khomutov <dmitry.khomutov@corp.mail.ru>
2016-02-10 13:48:59 +0300

    trackhashdao using of long2objectmap

--- fingerprint/src/main/java/angry1980/audio/dao/TrackHashDAOInMemoryImpl.java
+++ fingerprint/src/main/java/angry1980/audio/dao/TrackHashDAOInMemoryImpl.java
@@ -1,33 +1,42 @@
 package angry1980.audio.dao;
 
 import angry1980.audio.model.TrackHash;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 
 import java.util.*;
 
 public class TrackHashDAOInMemoryImpl implements TrackHashDAO {
 
-    private long mask;
-    private Map<Long, List<TrackHash>> index;
+    private final long mask;
+    private final Long2ObjectMap<List<TrackHash>> index;
 
     public TrackHashDAOInMemoryImpl(){
         this(-1);
     }
 
     public TrackHashDAOInMemoryImpl(long mask){
-        this.index = new HashMap<>();
+        this.index = new Long2ObjectOpenHashMap<>();
         this.mask = mask;
     }
 
     @Override
     public Optional<TrackHash> create(TrackHash hash) {
         if(hash != null){
-            index.computeIfAbsent(hash.getHash() & mask, l -> new ArrayList()).add(hash);
+            index.computeIfAbsent(getKey(hash.getHash()), l -> new ArrayList()).add(hash);
         }
         return Optional.ofNullable(hash);
     }
 
     @Override
     public List<TrackHash> findByHash(long hash) {
-        return index.getOrDefault(hash & mask, Collections.emptyList());
+        return index.getOrDefault(getKey(hash), Collections.emptyList());
+    }
+
+    private long getKey(long hash){
+        if(mask == -1){
+            return hash;
+        }
+        return hash & mask;
     }
 }

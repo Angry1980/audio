bfa908dec9984e40bf4f7d5f1466f43050f1744e
dmitry.khomutov <dmitry.khomutov@corp.mail.ru>
2016-02-16 11:55:23 +0300

    gist interval by percentile

--- app/src/main/java/angry1980/audio/CalculateCommonSimilaritiesGistWeights.java
+++ app/src/main/java/angry1980/audio/CalculateCommonSimilaritiesGistWeights.java
@@ -16,6 +16,8 @@ import org.springframework.context.annotation.Import;
 import rx.Subscriber;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
@@ -46,11 +48,11 @@ public class CalculateCommonSimilaritiesGistWeights {
                 //FingerprintType.LASTFM//,
                 //FingerprintType.PEAKS
         )){
-            int[] best = calculator.calculate(type)
-                    .flatMap(gist -> gist.getMaxInterval(10))
+            Interval best = calculator.calculate(type)
+                    .flatMap(gist -> gist.getInterval(70))
                     .orElse(null);
             if (best != null) {
-                LOG.info("Best weight values interval for {} is {}", type, Arrays.toString(best));
+                LOG.info("Best weight values interval for {} is {}", type, best);
             } else {
                 LOG.warn("It's not possible to calculate best interval for {}", type);
             }
@@ -85,37 +87,72 @@ public class CalculateCommonSimilaritiesGistWeights {
 
         final FingerprintType type;
         Int2IntSortedMap data;
+        int all;
 
         public Gist(FingerprintType type) {
             this.type = type;
             this.data = new Int2IntAVLTreeMap();
+            this.all = 0;
         }
 
-        public Optional<int[]> getMaxInterval(int count){
+        public Optional<Interval> getInterval(int percent){
             LOG.debug(data.toString());
-            return intervals(count)
-                    .max(Comparator.comparingInt(records -> records.stream().mapToInt(Map.Entry::getValue).sum()))
-                    .map(records -> records.stream().mapToInt(Map.Entry::getKey).toArray())
+            int limit = all * percent / 100;
+            LOG.debug("Limit is {}", limit);
+            return intervals(limit)
+                    //looking for an interval with min length
+                    .min(Comparator.comparingInt(Interval::getLength))
             ;
         }
 
-        private Stream<List<Map.Entry<Integer, Integer>>> intervals(int count){
-            IntFunction<List<Map.Entry<Integer, Integer>>> fetch = key -> fetchInterval(key, count);
+        private Stream<Interval> intervals(int limit){
+            Function<Integer, Optional<Interval>> fetch = key -> fetchInterval(key, limit);
             return data.keySet().stream()
-                    .mapToInt(k -> k)
-                    .mapToObj(fetch)
-                    .peek(records -> LOG.debug("Candidate to choose {}", records))
+                    .map(fetch)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .peek(interval -> LOG.debug("Candidate to choose {}", interval))
             ;
-
         }
 
-        private List<Map.Entry<Integer, Integer>> fetchInterval(int key, int count){
-            return data.tailMap(key).entrySet().stream().limit(count).collect(Collectors.toList());
+        private Optional<Interval> fetchInterval(int key, int limit){
+            int sum = 0;
+            for(Int2IntMap.Entry entry : data.tailMap(key).int2IntEntrySet()){
+                sum += entry.getIntValue();
+                if(sum >= limit){
+                    return Optional.of(new Interval(key, entry.getIntKey()));
+                }
+            }
+            return Optional.empty();
         }
 
         public void add(int value){
             data.computeIfPresent(value, (v, c) -> c + 1);
             data.computeIfAbsent(value, v -> 1);
+            all++;
         }
     }
+
+    private class Interval{
+        final int start;
+        final int finish;
+
+        public Interval(int start, int finish) {
+            this.start = start;
+            this.finish = finish;
+        }
+
+        public int getLength(){
+            return finish - start;
+        }
+
+        @Override
+        public String toString() {
+            return "Interval{" +
+                    "start=" + start +
+                    ", finish=" + finish +
+                    '}';
+        }
+    }
+
 }
